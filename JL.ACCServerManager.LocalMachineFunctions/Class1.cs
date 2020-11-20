6f6a39e68146ed8785df53fe4b96c4dd979f2508
Joseph Cullinan <joseph.cullinan@afrcorp.com>
2020-11-19 22:41:57 -0500

    Server

--- JL.ACCServerManager.LocalMachineFunctions/Class1.cs
+++ JL.ACCServerManager.LocalMachineFunctions/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace JL.ACCServerManager.LocalMachineFunctions
@@ -12,24 +13,66 @@ namespace JL.ACCServerManager.LocalMachineFunctions
     {
 
         private readonly IConfiguration Configuration;
+        private Process serverProcess;
+        private FileInfo serverExe;
 
 
         public LocalServerController(IConfiguration configuration)
         {
             Configuration = configuration;
             //see if server exists at location
-            var serverExe = new FileInfo(Configuration["ACCServerPath"] + Configuration["ACCServerExecutableName"] )
-            //see if server is running
-            
+            serverExe = new FileInfo(Configuration["ACCServerPath"] + Configuration["ACCServerExecutableName"]);
+            if (!serverExe.Exists) { throw new Exception($"Could not find server exe at {Configuration["ACCServerPath"]}{Configuration["ACCServerExecutableName"]}"); }
+            //see if server is running, and if so kill it. 
 
 
+
+            stopServer();
         }
 
 
-        public string restartServer()
+        public bool restartServer()
         {
+            stopServer();
+            startServer();
+            return true;
+        }
 
+        public bool stopServer()
+        {
+            Process[] processes = Process.GetProcessesByName(Configuration["ACCServerExecutableName"]);
+            foreach (Process p in processes)
+            {
+                p.Kill();
+            }
+            return true;
+        }
+        public void startServer()
+        {
+            serverProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = serverExe.FullName,
+                    Arguments = "",
+                    UseShellExecute = true,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            serverProcess.Start();
+            while (!serverProcess.StandardOutput.EndOfStream)
+            {
 
+                string line = serverProcess.StandardOutput.ReadLine();
+                parseOutputLines(line);
+
+            }
+        }
+
+        private void parseOutputLines(string outputLine)
+        {
+            Console.WriteLine(outputLine);
         }
     }
 }
